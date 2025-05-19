47f7a15eb91768a82ed4a688cd4aca5a36817ae1
alexesp <alexizaes@hotmail.com>
2025-05-19 02:02:19 +0200

    Crear dependencias

--- AspireBlazorApp1.AppHost/Program.cs
+++ AspireBlazorApp1.AppHost/Program.cs
@@ -1,5 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.AspireBlazorApp_ApiService>("aspireblazorapp-apiservice");
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.AspireBlazorApp_ApiService>("aspireblazorapp-apiservice");
+
+builder.AddProject<Projects.AspireBlazorApp_Web>("aspireblazorapp-web")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(apiService)
+    .WaitFor(apiService);
 
 builder.Build().Run();
